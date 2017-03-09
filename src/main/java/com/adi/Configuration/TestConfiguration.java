5874ea697ae4fc74941ae5c7c6f8c268a1d0445e
hyperion4040 <hyperion1987@wp.pl>
2017-03-09 16:11:34 +0100

    Configuration Bean was moved from PomApplication to another class.

---
+++ src/main/java/com/adi/Configuration/TestConfiguration.java
@@ -0,0 +1,42 @@
+package com.adi.Configuration;
+
+import com.adi.Model.Book;
+import com.adi.PomApplication;
+import com.adi.Repository.BookRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by hyperion on 09.03.17.
+ * This Configuration file was been created to move configuration bean from PomApplication class
+ */
+@Configuration
+public class TestConfiguration {
+
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(PomApplication.class);
+
+    @Bean
+    public CommandLineRunner demo(BookRepository repository) {
+        return (String... args) -> {
+            repository.save(new Book(1, "Lord of the rings", "Tolkien"));
+            repository.save(new Book(2, "Hobbit", "Tolkien"));
+            repository.save(new Book(3, "XXX", "YYY"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Book customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            log.info("Search for concrete author = Tolkien");
+            log.info("......................................");
+            for (Book tolkien : repository.findByAuthor("Tolkien")) {
+                log.info(tolkien.toString());
+            }
+        };
+    }
+}
--- src/main/java/com/adi/PomApplication.java
+++ src/main/java/com/adi/PomApplication.java
@@ -1,44 +1,19 @@
 package com.adi;
 
-import com.adi.Model.Book;
-import com.adi.Repository.BookRepository;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PomApplication {
 
-	private static final org.slf4j.Logger log = LoggerFactory.getLogger(PomApplication.class);
+//	private static final org.slf4j.Logger log = LoggerFactory.getLogger(PomApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PomApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner demo(BookRepository repository){
-		return (String... args) ->{
-			repository.save(new Book(1,"Lord of the rings","Tolkien"));
-			repository.save(new Book(2,"Hobbit","Tolkien"));
-			repository.save(new Book(3, "XXX", "YYY"));
 
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Book customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
-
-			log.info("Search for concrete author = Tolkien");
-			log.info("......................................");
-			for (Book tolkien : repository.findByAuthor("Tolkien")){
-				log.info(tolkien.toString());
-			}
-		};
 	}
 
 
-}
+
