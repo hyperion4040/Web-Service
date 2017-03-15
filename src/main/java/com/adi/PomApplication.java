d16d71bf8bc3455c9c211b5fd950638aa14580b7
hyperion4040 <hyperion1987@wp.pl>
2017-03-15 21:23:12 +0100

    Fixed problem with validation. Turn on @Configuration i TestConfiguration class.

--- src/main/java/com/adi/PomApplication.java
+++ src/main/java/com/adi/PomApplication.java
@@ -3,12 +3,15 @@ package com.adi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PomApplication {
 
 //	private static final org.slf4j.Logger log = LoggerFactory.getLogger(PomApplication.class);
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(PomApplication.class, args);
 	}
 
